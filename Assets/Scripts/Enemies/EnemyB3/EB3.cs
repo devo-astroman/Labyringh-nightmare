0ecbb58c34206fc0ed0c72d22a0deca2e36263e4
Devo Astroman <devo.astroman@gmail.com>
2026-03-03 14:15:26 +0100

    improve bullet hight

--- Assets/Scripts/Enemies/EnemyB3/EB3.cs
+++ Assets/Scripts/Enemies/EnemyB3/EB3.cs
@@ -169,7 +169,7 @@ public class EB3 : MonoBehaviour
             {
                 if (_enemyVision.CanSeePlayer(heroDetected.transform))
                 {
-                    EnemySawAction?.Invoke(heroDetected.transform.position+ new Vector3(0,1.5f,0));
+                    EnemySawAction?.Invoke(heroDetected.transform.position+ new Vector3(0,1.0f,0));
                 }
                 
             }
