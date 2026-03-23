d45519054b568ece48d389df225cd2bc0d9bb8ae
Devo Astroman <devo.astroman@gmail.com>
2026-03-23 11:01:39 +0100

    add settings ui graphics

---
+++ Assets/Scripts/Commons/AllGameData.cs
@@ -0,0 +1,14 @@
+public static class AllGameData 
+{
+
+    //settings
+    public static float defaultSfxVolumen;
+    public static float sfxVolumen;
+    public static float defaultMusicVolumen;
+    public static float musicVolumen;
+
+
+
+}
+
+
